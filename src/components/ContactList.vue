2959e34fc0ff298cf336b16644c6c3ab9320ad31
Mona Lisa <gugacns94@gmail.com>
2019-06-24 21:30:41 +0200

    second

---
+++ src/components/ContactList.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul>
+    <li v-for="(contact,index) in contacts" :key="index"> {{ contact.firstName }} {{ contact.lastName }} - {{ contact.email }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      contacts : [
+        { firstName: 'John', lastName: 'Doe', email: 'johndoe@example.com' },
+        { firstName: 'John', lastName: 'James', email: 'johnjames@example.com' },
+        { firstName: 'Marco', lastName: 'Tribiani', email: 'marcotribiani@example.com' },
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
