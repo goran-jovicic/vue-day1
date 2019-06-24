12455c454c682d4ee7e394a8e05516affdbeeefb
Mona Lisa <gugacns94@gmail.com>
2019-06-24 21:52:54 +0200

    interim objects

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <form>
+    <form @submit.prevent="addContact">
       <div>
         <label for="firstName">First Name : </label>
-        <input type="text" id="firstName"/>
+        <input type="text" id="firstName" v-model="newContact.firstName"/>
       </div>
       <div>
         <label for="lastName">Last Name : </label>
-        <input type="text" id="lastName"/>
+        <input type="text" id="lastName" v-model="newContact.lastName"/>
       </div>
       <div>
         <label for="email">Email : </label>
-        <input type="text" id="email"/>
+        <input type="text" id="email" v-model="newContact.email"/>
+      </div>
+      <div>
+        <button type="submit">Add contact</button>
       </div>
     </form>
     <ul>
@@ -26,11 +29,23 @@
 export default {
   data(){
     return {
+      newContact: {
+        firstName: '',
+        lastName: '',
+        email: ''
+      },
+
       contacts : [
         { firstName: 'John', lastName: 'Doe', email: 'johndoe@example.com' },
         { firstName: 'John', lastName: 'James', email: 'johnjames@example.com' },
         { firstName: 'Marco', lastName: 'Tribiani', email: 'marcotribiani@example.com' },
       ]
+    }
+  },
+
+  methods : {
+    addContact () {
+      this.contacts.push({...this.newContact})
     }
   }
 }
@@ -41,6 +56,11 @@ export default {
 
 ul {
   list-style-type: none;
+}
+
+input {
+  border-radius: 0.4rem;
+  margin: 0.2rem;
 }
 
 </style>
