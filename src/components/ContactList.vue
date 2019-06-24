df91924ac16b96cb3d0fd0503fb026fc94eb2b7d
Mona Lisa <gugacns94@gmail.com>
2019-06-24 21:38:15 +0200

    third

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,7 +1,25 @@
 <template>
-  <ul>
-    <li v-for="(contact,index) in contacts" :key="index"> {{ contact.firstName }} {{ contact.lastName }} - {{ contact.email }}</li>
-  </ul>
+  <div>
+    <form>
+      <div>
+        <label for="firstName">First Name : </label>
+        <input type="text" id="firstName"/>
+      </div>
+      <div>
+        <label for="lastName">Last Name : </label>
+        <input type="text" id="lastName"/>
+      </div>
+      <div>
+        <label for="email">Email : </label>
+        <input type="text" id="email"/>
+      </div>
+    </form>
+    <ul>
+      <li v-for="(contact,index) in contacts" :key="index">
+        {{ contact.firstName }} {{ contact.lastName }} - {{ contact.email }} 
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -20,5 +38,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+ul {
+  list-style-type: none;
+}
 
 </style>
